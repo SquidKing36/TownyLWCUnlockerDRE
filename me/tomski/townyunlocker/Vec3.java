d043c82521dc5a43ed90e2ffadb13caf46617b5d
Feuerstern <Feuerstern@users.noreply.github.com>
2014-10-05 00:20:12 +0200

    Create Vec3.java

---
+++ me/tomski/townyunlocker/Vec3.java
@@ -0,0 +1,26 @@
+package me.tomski.townyunlocker;
+
+public class Vec3
+{
+    private int x;
+    private int y;
+    private int z;
+    
+    public Vec3(final int x, final int z) {
+        super();
+        this.x = x;
+        this.z = z;
+    }
+    
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+    
+    public int getZ() {
+        return this.z;
+    }
+}
